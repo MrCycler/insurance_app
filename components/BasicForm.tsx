48220d98c795b6f8ca91efebd11180f8a147218d
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2021-01-10 13:35:36 -0500

    add: basic resposive behavior

--- components/BasicForm.tsx
+++ components/BasicForm.tsx
@@ -5,30 +5,48 @@ import "../styles/app.scss";
 import { Input } from "./Input.tsx";
 import { Button } from "./Button.tsx";
 
-
 export const BasicForm = (props) => {
   const [documentType, setdocumentType] = useState("");
   const [documentNro, setdocumentNro] = useState("");
   const [date, setDate] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [active, setActive] = useState(false);
+
   useEffect(() => {
-    if ((documentNro.length==8)&&(phoneNumber.length==9)){
-      setActive(true)
+    if (documentNro.length == 8 && phoneNumber.length == 9) {
+      setActive(true);
     }
-  }, [documentType,documentNro,date,phoneNumber])
+  }, [documentType, documentNro, date, phoneNumber]);
 
   return (
     <div className="form">
-      <div  className="form__title">
+      <div className="form__title">
         <p>Obtén tu&nbsp;</p>
         <p className="form__title__active">seguro ahora</p>
       </div>
       <p className="form__subtitle">Ingresa los datos para comenzar</p>
-      <Input label="Nro de documento" type="tel" setValue={setdocumentNro} value={documentNro} maxLength="8"/>
-      <Input label="Fecha de nacimiento" type="text" setValue={setDate} value={date} maxLength="14"/>
-      <Input label="Celular" type="tel" setValue={setPhoneNumber} value={phoneNumber} maxLength="9"/>
-      <Button msg="COMENCEMOS" active={active}/>
+      <Input
+        label="Nro de documento"
+        type="tel"
+        setValue={setdocumentNro}
+        value={documentNro}
+        maxLength="8"
+      />
+      <Input
+        label="Fecha de nacimiento"
+        type="text"
+        setValue={setDate}
+        value={date}
+        maxLength="14"
+      />
+      <Input
+        label="Celular"
+        type="tel"
+        setValue={setPhoneNumber}
+        value={phoneNumber}
+        maxLength="9"
+      />
+      <Button msg="COMENCEMOS" active={active} />
     </div>
   );
 };
