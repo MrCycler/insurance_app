9604256dfa240076354b6e303ef8973a5b73c769
Juan Mendoza <mendoza.jm@pucp.edu.pe>
2021-01-10 22:33:45 -0500

    edit: Completed Benefits Banner

--- components/PlanBanner.tsx
+++ components/PlanBanner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/app.scss";
 
-
+import {AiFillHeart} from "react-icons/ai";
 
 export const PlanBanner = (props) => {
   return (
@@ -14,12 +14,12 @@ export const PlanBanner = (props) => {
         </div>
         <div className="plan_banner__img"><img src="/images/Illustration.png" /></div>
         <div className="plan_banner__benefits">
-            <p className={props.plan.benefits_num>0?"":"plan_banner__benefits__deactive"}><b>Lima</b> (zona de cobertura)</p>
-            <p className={props.plan.benefits_num>1?"":"plan_banner__benefits__deactive"}><b>+30 clínicas</b> (en red afiliada)</p>
-            <p className={props.plan.benefits_num>2?"":"plan_banner__benefits__deactive"}>Médico a domicilio</p>
-            <p className={props.plan.benefits_num>3?"":"plan_banner__benefits__deactive"}>Chequeos preventivos</p>
-            <p className={props.plan.benefits_num>4?"":"plan_banner__benefits__deactive"}>Reembolso nacional</p>
-            <p className={props.plan.benefits_num>5?"":"plan_banner__benefits__deactive"}>Reembolso internacional</p>
+            <p className={props.plan.benefits_num>0?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> <b>Lima</b> (zona de cobertura)</p>
+            <p className={props.plan.benefits_num>1?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> <b>+30 clínicas</b> (en red afiliada)</p>
+            <p className={props.plan.benefits_num>2?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> Médico a domicilio</p>
+            <p className={props.plan.benefits_num>3?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> Chequeos preventivos</p>
+            <p className={props.plan.benefits_num>4?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> Reembolso nacional</p>
+            <p className={props.plan.benefits_num>5?"":"plan_banner__benefits__deactive"}> <AiFillHeart/> Reembolso internacional</p>
         </div>
 
     </div>
